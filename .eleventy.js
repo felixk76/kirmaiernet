731f879df0663d807e4af41da0a319a14e5189d4
Felix Kirmaier <f.kirmaier@saint-elmos.com>
2020-08-23 16:32:43 +0200

    JS

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 module.exports = config => {
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/Images/');
+    config.addPassthroughCopy('./src/Fonts/');
     return {
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',  
+        htmlTemplateEngine: 'njk',
         dir: {
             input: 'src',
             output: 'dist'
