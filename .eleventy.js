5e302416db05faee9bc3655b66fdbe1a572200d8
Felix Kirmaier <f.kirmaier@saint-elmos.com>
2020-09-05 18:08:16 +0200

    Add Links to Social Media Accounts

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,14 @@ module.exports = config => {
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/Images/');
     config.addPassthroughCopy('./src/Fonts/');
+    config.addPassthroughCopy('./src/*.ico');
+    config.addPassthroughCopy('./src/*.png');
+    config.addPassthroughCopy('./src/browserconfig.xml');
 
     const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+    const svgContents = require("eleventy-plugin-svg-contents");
     config.addPlugin(eleventyNavigationPlugin);
+    config.addPlugin(svgContents);
 
     config.addCollection('footer', collection => {
         return collection
